82eae4bdb7a643e32df05d0d14541b4bdbc8a2e0
Bintang <bintangadiputrapratama@gmail.com>
2024-05-09 14:19:04 +0700

    refactor: update.sh

--- update.sh
+++ update.sh
@@ -1,14 +1,26 @@
 #!/usr/bin/env bash
 
-printf '\033[1mUpdating flake inputs...\n'
-nix flake update --accept-flake-config
+case "$1" in
+"input")
+  printf '\033[1mUpdating flake inputs...\n'
+  nix flake update --accept-flake-config
+;;
 
-printf '\033[1mPushing inputs to cachix...\n'
-nix flake archive --accept-flake-config --json \
-  | jq -r '.path,(.inputs|to_entries[].value.path)' \
-  | cachix push spitulax
+"build")
+  nix build .#all --accept-flake-config
+;;
 
+"push-input")
+  printf '\033[1mPushing inputs to cachix...\n'
+  nix flake archive --accept-flake-config --json \
+    | jq -r '.path,(.inputs|to_entries[].value.path)' \
+    | cachix push spitulax
+;;
+
+"push-pkgs")
 printf '\033[1mBuilding packages...\n'
 nix build .#all --accept-flake-config --json \
   | jq -r '.[].outputs | to_entries[].value' \
   | cachix push spitulax
+;;
+esac
