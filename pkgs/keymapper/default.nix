46531b08863db527e4a6dd48d4c3a3cc48518d33
Bintang <bintangadiputrapratama@gmail.com>
2024-05-19 14:53:24 +0700

    update several packages
    - hyprlock: 316c377 -> 997f222
    - keymapper: 4.3.0 -> 4.3.1
    - waybar: 912d0e8 -> 8ad7c75

--- pkgs/keymapper/default.nix
+++ pkgs/keymapper/default.nix
@@ -1,14 +1,14 @@
 { pkgs }:
 pkgs.keymapper.overrideAttrs (newAttrs: oldAttrs: {
-  version = "4.3.0";
+  version = "4.3.1";
   src = pkgs.fetchFromGitHub {
     owner = "houmain";
     repo = "keymapper";
     rev = newAttrs.version;
-    hash = "sha256-QfISsRm0j/VoTmpDQes5XQooXHcmjqRD/WS/nvPDl00=";
+    hash = "sha256-SjYPkcXBEHjK7zgwL6U4ltjvhKuGMZWbp55LtndU400=";
   };
   nativeBuildInputs = oldAttrs.nativeBuildInputs ++ (with pkgs; [
     gtk3
-    libappindicator
+    libayatana-appindicator
   ]);
 })
