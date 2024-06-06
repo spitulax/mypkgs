1e137af5e2defc1397f97ec74548d0db12752a66
Bintang <bintangadiputrapratama@gmail.com>
2024-06-06 22:53:59 +0700

    odin: llvm 13 -> 17

--- pkgs/odin/default.nix
+++ pkgs/odin/default.nix
@@ -1,7 +1,15 @@
 { pkgs
 , src
 }:
-pkgs.odin.overrideAttrs (newAttrs: oldAttrs: {
+(pkgs.odin.override {
+  llvmPackages_13 = pkgs.llvmPackages_17;
+}).overrideAttrs (newAttrs: oldAttrs: {
   version = "dev-2024-06" + "_" + (src.shortRev or "dirty");
   inherit src;
+
+  preBuild = ''
+    cd vendor/stb/src
+    make
+    cd ../../..
+  '';
 })
