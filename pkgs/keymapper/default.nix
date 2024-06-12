1cd539c40c2de88267d79e70d0feb75a98886d9e
Bintang <bintangadiputrapratama@gmail.com>
2024-06-13 00:22:46 +0700

    update several packages

--- pkgs/keymapper/default.nix
+++ pkgs/keymapper/default.nix
@@ -1,11 +1,11 @@
 { pkgs }:
 pkgs.keymapper.overrideAttrs (newAttrs: oldAttrs: {
-  version = "4.3.1";
+  version = "4.4.0";
   src = pkgs.fetchFromGitHub {
     owner = "houmain";
     repo = "keymapper";
     rev = newAttrs.version;
-    hash = "sha256-SjYPkcXBEHjK7zgwL6U4ltjvhKuGMZWbp55LtndU400=";
+    hash = "sha256-NB9sVSkd01lm9Ia8fGrnICjD1cNdPfcvJ++Yy3NO5QQ=";
   };
   nativeBuildInputs = oldAttrs.nativeBuildInputs ++ (with pkgs; [
     libayatana-appindicator
