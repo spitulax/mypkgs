be885c0a872b8d00bf8cdb574831b0f475b54026
Bintang Adiputra Pratama <bintangadiputra@proton.me>
2025-04-11 20:42:31 +0700

    pkgs/gplates: fix for Wayland

--- pkgs/gplates/default.nix
+++ pkgs/gplates/default.nix
@@ -3,6 +3,8 @@
 , fetchurl
 , utils
 , symlinkJoin
+, runCommand
+, makeBinaryWrapper
 }:
 let
   iconDrv = utils.archiveTools.extractZip {
@@ -21,12 +23,27 @@ let
     exec = "${gplates}/bin/gplates %U";
     icon = "${iconDrv}/newlogo.svg";
   };
+
+  # Wayland fix
+  # https://discourse.gplates.org/t/release-gplates-as-snap-or-flatpak-for-linux/696/2?u=john.cannon
+  gplates-wrapped = runCommand gplates.name
+    {
+      inherit (gplates) pname version meta;
+      nativeBuildInputs = [ makeBinaryWrapper ];
+    }
+    ''
+      mkdir -p $out/bin
+      ln -s ${gplates}/share $out/share
+      ln -s ${gplates}/bin/.gplates-wrapped $out/bin/.gplates-wrapped
+      makeBinaryWrapper ${gplates}/bin/gplates $out/bin/gplates \
+        --set QT_QPA_PLATFORM xcb
+    '';
 in
 symlinkJoin {
   pname = "gplates";
   inherit (gplates) version meta;
   paths = [
-    gplates
+    gplates-wrapped
     desktopItem
   ];
 }
